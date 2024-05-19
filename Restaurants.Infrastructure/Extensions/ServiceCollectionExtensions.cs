17a521d2b399185e52e0fa610702afb901a6d513
wijde <wijde@DESKTOP-98UO67M>
2024-05-19 12:01:21 +0100

    Ajoutez des fichiers projet.

---
+++ Restaurants.Infrastructure/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Protocols;
+using Restaurants.Infrastructure.Persistence;
+using Restaurants.Infrastructure.Seeders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restaurants.Infrastructure.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static void AddInfrastructure(this IServiceCollection services,IConfiguration configuration) 
+        {
+            var connectionString = configuration.GetConnectionString("RestaurantDb");
+            services.AddDbContext<RestaurantsDbContext>(options => options.UseSqlServer(connectionString));
+            services.AddScoped<IRestaurantSeeder, RestaurantSeeder>();
+        }
+    }
+}
---
+++ Restaurants.Infrastructure/Migrations/20240519083814_updateTables.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Restaurants.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class updateTables : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Address_Strret",
+                table: "Restaurants",
+                newName: "Address_Street");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "Dishes",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "Dishes");
+
+            migrationBuilder.RenameColumn(
+                name: "Address_Street",
+                table: "Restaurants",
+                newName: "Address_Strret");
+        }
+    }
+}
